083d3f008b0431a776004f227ee168e9d215be73
Przemysław Basiaga <przemek.basiaga23@gmail.com>
2020-11-04 19:49:57 +0100

    implement BlockOfClasses Entity

---
+++ src/main/java/com/sda/zd18/gr4/model/BlockOfClasses.java
@@ -0,0 +1,24 @@
+package com.sda.zd18.gr4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class BlockOfClasses {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private List<Lesson> lessons;
+
+}

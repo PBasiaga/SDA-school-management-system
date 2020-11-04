9c30415dea3b2f15bfe1b079d5ed14e8d0e7f684
Przemysław Basiaga <przemek.basiaga23@gmail.com>
2020-11-04 19:32:08 +0100

    redevelop User Entity delted by gitignore fix

--- src/main/java/com/sda/zd18/gr4/model/User.java
+++ src/main/java/com/sda/zd18/gr4/model/User.java
@@ -1,2 +1,30 @@
-package com.sda.zd18.gr4.model;public class User {
+package com.sda.zd18.gr4.model;
+
+
+import com.sda.zd18.gr4.utility.UserType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+    private UserType userType;
+    private Boolean isActive;
+
+
 }
